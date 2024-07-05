0b37dbba897ab39b923cc327eefaa844bedd4390
ahmadelham21 <ahmadelham21@gmail.com>
2024-07-05 09:44:03 +0700

    [ADD] update Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css'
-import Navbar from './components/Navbar'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Login from "./pages/auth/Login";
 
 const App = () => {
   return (
     <>
-      <Navbar />
+      <Login />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
